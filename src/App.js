f8fa21af05934ab00f41021fd2a2cf7b3b470c17
HideriLavanette <110430190+HideriLavanette@users.noreply.github.com>
2023-03-17 23:16:35 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text'>
+        <h1>Ашот-пашот сделал ваншот</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
